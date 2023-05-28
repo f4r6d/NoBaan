14ef20d24170754550c29e5ca2d6d5eea9904071
Farshid Gholizadeh <farshidelf@yahoo.com>
2023-05-28 12:58:22 +0330

    change config

--- bootstrap/config.php
+++ bootstrap/config.php
@@ -21,8 +21,8 @@ define('CACHE_TTL', 3600);
 define('CACHE_KEY', 'PRODUCT_LIST');
 
 // fill redis uri or fill the others; they are the same
-// define('REDIS_URI', 'tcp://127.0.0.1:6379');
-define('REDIS_URI', 'redis://farshid:Nobaan123-@redis-19330.c266.us-east-1-3.ec2.cloud.redislabs.com:19330');
+define('REDIS_URI', 'tcp://127.0.0.1:6379');
+// define('REDIS_URI', 'redis://farshid:Nobaan123-@redis-19330.c266.us-east-1-3.ec2.cloud.redislabs.com:19330');
 define('REDIS_SCHEME', 'tcp');
 define('REDIS_HOST', '127.0.0.1');
 define('REDIS_PORT', '6379');
